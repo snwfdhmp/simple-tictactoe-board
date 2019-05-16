e1dcbc787b1b15800b0ad6f4a9d01a969b4d899f
Martin Joly <snwfdhmp@gmail.com>
2019-05-16 10:19:10 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,20 +7,15 @@ import (
 
 func main() {
 	var field [3][3]int
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			formToInt := func(key string) int {
 				return int(r.FormValue(key)[0] - '0')
 			}
-
 			x, y := formToInt("x"), formToInt("y")
 			field[x][y] = (field[x][y] + 1) % 3
 		}
-
 		json.NewEncoder(w).Encode(field)
-
 	})
-
 	http.ListenAndServe(":8081", nil)
 }
